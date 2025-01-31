27a5ded4a0473af73013d901c657148b038a16b3
löjromslyx <emilholger06@gmail.com>
2025-01-31 09:51:42 +0100

    kommentar

--- imageSlider/js/main.js
+++ imageSlider/js/main.js
@@ -1,11 +1,11 @@
-
+//sparar .prev och .next i variabeln knapp
 let knapp=$(".prev, .next")
-
+//visar knapp om javascript är aktiverat
 knapp.show();
 
-
+//skapar en funktion till slidern
 $(document).ready(function() {
-    
+    //om man klickar pil åt höger så aktiveras funktionen, här flyttar den bak bilden och lägger fram nästa bild
     $('.next').on('click', function() {
         var currentImg = $('.active');
         var nextImg = currentImg.next();
@@ -15,7 +15,7 @@ $(document).ready(function() {
             nextImg.addClass('active').css('z-index', 10);
         }
     });
-
+    //om man klickar pil åt vänster så aktiveras funktionen, här flyttar den bak bilden och lägger fram förekommande bild
     $('.prev').on('click', function() {
         var currentImg = $('.active');
         var prevImg = currentImg.prev();
@@ -27,23 +27,35 @@ $(document).ready(function() {
     });
 });
 
+
+//radiobuttons
+
+//sparar dots-container till variabel dots
 let dots=$(".dots-container")
+//visar dot när javascirpt är aktiverat
 dots.show();
 
+//hämtar och sparar prickarna till slide show
 const slide1 = document.getElementById("dot1");
 const slide2 = document.getElementById("dot2");
 const slide3 = document.getElementById("dot3");
 const slide4 = document.getElementById("dot4");
 
+//sparar alla slides i en array
 let radio_array = [slide1, slide2, slide3, slide4]; 
+//kollar längden på arrayen
 let radio_length = radio_array.length;
 
+//sparar variabel i som 0
 let i = 0;
+//kollar om array = 0 för att veta första sliden
 radio_array[i].checked = true; 
 
+//sparar tag .next och .prev  
 const right = document.querySelector(".next"); 
 const screenLeft = document.querySelector(".prev");
 
+//om man klickar till höger så lägger den på +1 på slidern, dvs flyttar pricken åt höger
 right.onclick = () => {
     i++;
     if (i < 0) {
@@ -51,6 +63,7 @@ right.onclick = () => {
       radio_array[i].checked = true;
 };
 
+//om man klickar till vänster så lägger den på -1 på slidern, dvs flyttar pricken åt vänster
 screenLeft.onclick = () => {
     i--;
     if (i < 0) {
