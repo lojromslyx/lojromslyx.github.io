fc65411c2b75058c226bffda86a8e883dd767988
löjromslyx <emilholger06@gmail.com>
2025-05-19 15:13:20 +0200

    smallchanges

--- BilensUtveckling/Java/TextAnim.js
+++ BilensUtveckling/Java/TextAnim.js
@@ -1,9 +1,10 @@
-var text = document.getElementById('text');
-var originalText = text.innerText;
-var words = originalText.split(' ');
-var animationDelay = 90;
+var text = document.getElementById('text'); //hämtar elemtet med id "text"
+var originalText = text.innerText; //hämtar innehållet och sparar den i ny variabel
+var words = originalText.split(' '); //delar upp texten i en arrey av ord
+var animationDelay = 90; //bestämmer hur mycket fördröjning som ska vara mellan varje varje ords animation
 var newDom = '';
 
+//Varje ord får en individuell animation-delay som ökar med i * 90 ms – så att orden animeras ett efter ett.
 for (let i = 0; i < words.length; i++) {
     newDom += '<span class="char" style="animation-delay:' + (i * animationDelay) + 'ms">' + words[i] + '</span> ';
 }
--- BilensUtveckling/Java/slider.js
+++ BilensUtveckling/Java/slider.js
@@ -1,11 +1,11 @@
 let currentSlide = 0;
-const slides = document.querySelectorAll('.slider-inner img');
-const totalSlides = slides.length;
+const slides = document.querySelectorAll('.slider-inner img'); //bilden hämtas
+const totalSlides = slides.length; //räknas hur många bilder det finns totalt
 
 function showNextSlide() {
-    slides[currentSlide].classList.remove('active');
-    currentSlide = (currentSlide + 1) % totalSlides;
-    slides[currentSlide].classList.add('active');
+    slides[currentSlide].classList.remove('active'); //tar bort klassen active från den nuvarande bilden
+    currentSlide = (currentSlide + 1) % totalSlides; //ökar curentslide med 1 
+    slides[currentSlide].classList.add('active'); // lägger till klassen active på den nya bilden
 }
 
 // Byt bild var 4:e sekund
